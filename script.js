5daaf52ba06d5f9340aae67a68fd5b6e092dbb42
Solar <vanceluke42@gmail.com>
2025-03-01 12:48:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// Example: Add to cart button functionality
+// Example: Add to cart button functionality with smooth animation
 document.querySelectorAll('.item-card button').forEach(button => {
     button.addEventListener('click', function() {
-        alert('Item added to cart!');
+        this.innerText = "Added to Cart!";
+        this.style.backgroundColor = "#2ecc71";
+        setTimeout(() => {
+            this.innerText = "Add to Cart";
+            this.style.backgroundColor = "#27ae60";
+        }, 2000);
     });
 });
