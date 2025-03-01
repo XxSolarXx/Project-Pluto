b0ff4b2e6b94cee526c6445b35b94e26bac51c51
Solar <vanceluke42@gmail.com>
2025-03-01 12:25:57 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Add to cart button functionality
+document.querySelectorAll('.item-card button').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('Item added to cart!');
+    });
+});
